8654ee6f78194ba36d0b3bd5af5c6a4690fe95cb
U-stack-ux <contato@upscore.com>
2026-05-19 21:35:28 -0300

    fix: limpa importacoes nao utilizadas em security.go

--- backend/security.go
+++ backend/security.go
@@ -5,7 +5,6 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
-	"fmt"
 	"io"
 )
 
@@ -14,10 +13,14 @@ var key = []byte("sua-chave-ultra-secreta-32-bytes!")
 
 func Encrypt(text string) (string, error) {
 	block, err := aes.NewCipher(key)
-	if err != nil { return "", err }
+	if err != nil {
+		return "", err
+	}
 	ciphertext := make([]byte, aes.BlockSize+len(text))
 	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil { return "", err }
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(text))
 	return base64.URLEncoding.EncodeToString(ciphertext), nil
@@ -26,7 +29,9 @@ func Encrypt(text string) (string, error) {
 func Decrypt(cryptoText string) (string, error) {
 	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
 	block, err := aes.NewCipher(key)
-	if err != nil { return "", err }
+	if err != nil {
+		return "", err
+	}
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
 	stream := cipher.NewCFBDecrypter(block, iv)
